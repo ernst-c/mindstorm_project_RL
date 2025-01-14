fab33bb5bd27597c4fad1d08954c06951a479c8e
ernst_c <e.l.cancrinus@gmail.com>
2025-01-14 17:07:25 +0100

    initial implementation RK4 for dynamics

---
+++ EOM/RK4.py
@@ -0,0 +1,20 @@
+#x,wheel_velocities,u,param
+def runge_kutta4(x, wheel_velocities, u, eom, param):
+
+    output_k1 = eom(x, wheel_velocities, u, param)
+    k1 = output_k1[:4]  # First 4 elements are for position and orientation
+    wheel_velocities_k1 = output_k1[4:]  # Last 2 elements are for wheel velocity derivatives
+
+    output_k2 = eom(x + 0.5 * param[2] * k1,wheel_velocities+ 0.5 * param[2] * wheel_velocities_k1, u, param)
+    k2 = output_k2[:4]  # First 4 elements are for position and orientation
+    wheel_velocities_k2 = output_k2[4:]  # Last 2 elements are for wheel velocity derivatives
+
+    output_k3 = eom(x + 0.5 * param[2] * k2,wheel_velocities+ 0.5 * param[2] * wheel_velocities_k2, u, param)
+    k3 = output_k3[:4]  # First 4 elements are for position and orientation
+    wheel_velocities_k3 = output_k3[4:]  # Last 2 elements are for wheel velocity derivatives
+
+    output_k4 = eom(x + param[2] * k3,wheel_velocities + param[2] * wheel_velocities_k3, u, param)
+    k4 = output_k4[:4]  # First 4 elements are for position and orientation
+    wheel_velocities_k4 = output_k4[4:]  # Last 2 elements are for wheel velocity derivatives
+
+    return (param[2] / 6) * (k1 + 2 * k2 + 2 * k3 + k4), (param[2] / 6) * (wheel_velocities_k1 + 2 * wheel_velocities_k2 + 2 * wheel_velocities_k3 + wheel_velocities_k4)
--- EOM/eom.py
+++ EOM/eom.py
@@ -10,10 +10,10 @@ def cont_model(x,wheel_velocities,u,param):
     #scale u to motor input range
     scaled_u = u*100
     # Motor inputs (PWM commands for left and right motors)
-    l_omega, r_omega = ((scaled_u*12 - np.array([wheel_vel_l,wheel_vel_l]))/0.25)*param[2] # degrees/second 
+    l_domega, r_domega = ((scaled_u*12 - np.array([wheel_vel_l,wheel_vel_l]))/0.25)*param[2] # degrees/second 
     
-    v_l = (l_omega+wheel_vel_l) * param[1] * 1/180*pi  # m/s 
-    v_r = (r_omega+wheel_vel_r) * param[1] * 1/180*pi  # m/s 
+    v_l = (l_domega+wheel_vel_l) * param[1] * 1/180*pi  # m/s 
+    v_r = (r_domega+wheel_vel_r) * param[1] * 1/180*pi  # m/s 
 
     v = (v_l + v_r) / 2  # Linear velocity
     omega = (v_l - v_r) / param[0]  # Angular velocity
@@ -22,6 +22,6 @@ def cont_model(x,wheel_velocities,u,param):
     y_dot = v * np.cos(alpha) * param[2]
     alpha_dot = omega * param[2]
     
-    dx = [x_dot, y_dot, alpha_dot, 0, wheel_vel_l, wheel_vel_r]
+    dx = [x_dot, y_dot, alpha_dot, 0, l_domega, r_domega]
 
     return dx
