e185f4538e8df8ac7c84283bae553bda0a3d075e
ernst_c <e.l.cancrinus@gmail.com>
2025-01-15 11:28:53 +0100

    small eval env code change

--- Eval_Environments/mindstormBot.py
+++ Eval_Environments/mindstormBot.py
@@ -18,7 +18,7 @@ class mindstormBotEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 10}
 
     def __init__(self, goal_state=np.array([0, 1.45, 0, 0, 0], dtype=float),
-                 episode_steps=1000, rewardfunc=sparse_reward2d, eom=cont_model, render_mode=None, param=np.array([0.3,0.2,0.1])):
+                 episode_steps=1000, rewardfunc=sparse_reward2d, eom=cont_model, render_mode=None, param=np.array([0.3,0.2,0.125])):
 
         #rendering
         assert render_mode is None or render_mode in self.metadata["render_modes"]
